53fd17bcf349637c499486c086bf59d662183ce8
Tatiana Polyanitsa <gziltoid@Tatianas-MacBook-Pro.local>
2019-04-05 22:47:29 -0700

    created project "sandbox"

---
+++ sandbox/src/main/java/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+
+    public static void main(String[] args) {
+//        byte a;
+//        a = 127;
+//
+//        short b;
+//        b = 20;
+//
+//        int c;
+//        c = 50;
+//
+//        int age;
+//        age = 30;
+//
+//        boolean bool;
+//        bool = true;
+//
+//        char ch = 'a';
+
+//        String firstName = "John";
+//        String lastName = "Doe";
+//
+//        int age = 50;
+//        int x = 5;
+//        int y = 3;
+//        int z = x + y;
+        int age = 30;
+        String text = "My age is";
+        System.out.println(text + ' ' + age);
+    }
+}
