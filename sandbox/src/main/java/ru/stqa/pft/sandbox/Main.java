dfaf200ca3e7248a8a67e14ef4ed658cdaaf5c38
Tatiana Polyanitsa <gziltoid@Tatianas-MacBook-Pro.local>
2019-04-09 16:48:29 -0700

    implemented class Square

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Main.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Main.java
@@ -3,32 +3,12 @@ package ru.stqa.pft.sandbox;
 public class Main {
 
     public static void main(String[] args) {
-//        byte a;
-//        a = 127;
-//
-//        short b;
-//        b = 20;
-//
-//        int c;
-//        c = 50;
-//
-//        int age;
-//        age = 30;
-//
-//        boolean bool;
-//        bool = true;
-//
-//        char ch = 'a';
-
-//        String firstName = "John";
-//        String lastName = "Doe";
-//
-//        int age = 50;
-//        int x = 5;
-//        int y = 3;
-//        int z = x + y;
-        int age = 30;
-        String text = "My age is";
-        System.out.println(text + ' ' + age + " fuck");
+        //hello("Tatiana");
+        Square s = new Square(5);
+        System.out.println(s.area());
     }
+
+//    public static void hello(String somebody) {
+//        System.out.println("Hello, " + somebody + "!");
+//    }
 }
