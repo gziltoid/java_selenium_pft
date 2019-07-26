a3636c71930e5d477667d0eff28fb2b07859cdd2
Tatiana Polyanitsa <gziltoid@Tatianas-MacBook-Pro.local>
2019-07-25 23:40:20 -0700

    changed comparator for groups

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -30,8 +30,10 @@ public class GroupCreationTests extends TestBase {
 //        max = g.getId();
 //      }
 //    }
+//    group.setId(max);
+//    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
 
-    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
+    group.setId(after.stream().max(Comparator.comparingInt(GroupData::getId)).get().getId());
     before.add(group);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
